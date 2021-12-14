bf651408644c04b09a117cf9fb8ba103c28906d7
Matteo Chen <kaiximatteo.chen@mail.polimi.it>
2021-12-14 01:07:56 +0100

    added new macros

--- common/config.h
+++ common/config.h
@@ -1,9 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/* window param */
 #define WINDOW_HEIGH    480
 #define WINDOW_LENGTH   640
 
+/* font param */
+#define FONT_DIR        "../../fonts/monaco.ttf"
+#define FONT_SIZE       24
 
 
 #endif
